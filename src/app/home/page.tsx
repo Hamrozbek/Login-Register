82ad748325ea613ccf5f364959ad89d73d28149f
Hamroz Nuriddinov <hamroznuriddinov41@gmail.com>
2026-02-13 17:01:16 +0500

    login and register and home page update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -17,14 +17,15 @@ export default function Home() {
     }, []);
 
     const handleLogout = () => {
-        // Custom toast confirm in English
         toast(
             t => (
-                <div className="flex flex-col gap-2">
-                    <p className="text-white">Are you sure you want to logout?</p>
-                    <div className="flex justify-center gap-2">
+                <div className="flex flex-col gap-3 sm:gap-2">
+                    <p className="text-white text-sm sm:text-md">
+                        Are you sure you want to logout?
+                    </p>
+                    <div className="flex justify-center gap-3 sm:gap-2">
                         <button
-                            className="bg-cyan-400 cursor-pointer text-black px-3 py-1 rounded-full"
+                            className="bg-cyan-400 cursor-pointer text-black px-4 py-1 md:px-6 md:py-2 rounded-full text-sm sm:text-xs"
                             onClick={() => {
                                 localStorage.removeItem("rememberedUsername");
                                 toast.dismiss(t.id);
@@ -34,7 +35,7 @@ export default function Home() {
                             Yes
                         </button>
                         <button
-                            className="bg-gray-700 cursor-pointer text-white px-3 py-1 rounded-full"
+                            className="bg-gray-700 cursor-pointer text-white px-4 py-1 md:px-6 md:py-2  rounded-full text-sm sm:text-xs"
                             onClick={() => toast.dismiss(t.id)}
                         >
                             No
@@ -47,13 +48,17 @@ export default function Home() {
     };
 
     return (
-        <section className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-4">
+        <section className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-4 sm:px-2">
             <Toaster position="top-center" />
-            <h1 className="text-3xl font-bold mb-4">Welcome, {username}!</h1>
-            <p className="mb-8 text-gray-300">You are now logged in.</p>
+            <h1 className="text-3xl md:text-3xl lg:text-4xl font-bold mb-2 sm:mb-3 text-center">
+                Welcome, {username}!
+            </h1>
+            <p className="mb-4 text-gray-300 text-lg md:text-xl text-center sm:text-base">
+                You are now logged in.
+            </p>
             <button
                 onClick={handleLogout}
-                className="py-2 px-6 cursor-pointer rounded-full border border-cyan-400 text-cyan-400 hover:bg-cyan-400 hover:text-black transition duration-300 shadow-[0_0_25px_rgba(0,255,255,0.7)]"
+                className="py-2 px-8 md:py-3 md:px-10 sm:py-2 sm:px-6 cursor-pointer rounded-full border border-cyan-400 text-cyan-400 hover:bg-cyan-400 hover:text-black transition duration-300 shadow-[0_0_25px_rgba(0,255,255,0.7)] text-md md:text-lg sm:text-base"
             >
                 Logout
             </button>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -24,21 +24,37 @@ export default function Register() {
     return (
         <section className="min-h-screen flex items-center justify-center px-4">
             <Toaster position="top-right" />
-            <div className="w-[380px] rounded-3xl backdrop-blur-[0.5px] border border-cyan-400/40 shadow-[0_0_40px_rgba(0,255,255,0.4)] p-10 text-white">
+            <div className="max-w-md w-full rounded-3xl backdrop-blur-[0.5px] border border-cyan-400/40 shadow-[0_0_40px_rgba(0,255,255,0.4)] p-10 text-white">
                 <div className="text-center mb-8">
                     <h1 className="text-3xl font-bold text-cyan-400">REGISTER</h1>
                     <p className="text-gray-300 mt-2">Create Account</p>
                 </div>
                 <form className="flex flex-col gap-6" onSubmit={handleRegister}>
-                    <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}
-                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400" />
-                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}
-                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400" />
-                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}
-                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400" />
-                    <button type="submit"
-                        className="mt-4 py-2 rounded-full border border-cyan-400 text-cyan-400 hover:bg-cyan-400 hover:text-black transition duration-300 shadow-[0_0_25px_rgba(0,255,255,0.7)]">
-                        SIGN UP
+                    <input type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400 sm:text-sm md:text-base"
+                    />
+
+                    <input type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400 sm:text-sm md:text-base"
+                    />
+
+                    <input type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        className="bg-transparent focus:bg-transparent border-b border-cyan-400 py-2 px-4 rounded-full focus:outline-none placeholder-gray-400 sm:text-sm md:text-base"
+                    />
+
+                    <button
+                        type="submit"
+                        className="mt-4 py-3 px-8 rounded-full border border-cyan-400 text-cyan-400 hover:bg-cyan-400 hover:text-black transition duration-300 shadow-[0_0_25px_rgba(0,255,255,0.7)] sm:py-2 sm:px-4"
+                    >   SIGN UP
                     </button>
                 </form>
                 <p className="text-center text-sm text-gray-300 mt-6">
